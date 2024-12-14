951677b2e5df9e97e448bc523e1bb8534f435f15
dbs92590 <dbs92590@gmail.com>
2024-12-13 20:44:25 -0600

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,7 +3,12 @@ package day01;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        
         System.out.println("hey");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+
 
     }
 }
